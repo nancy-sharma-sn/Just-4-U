29b2b9c6ad7b175c47ea111b76f39b0f66417838
nancy-sharma-sn <69963447+nancy-sharma-sn@users.noreply.github.com>
2022-05-19 05:58:53 +0530

    folders

---
+++ app/src/main/java/com/just4u/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.just4u.shopping.Prevalent;
+
+import com.just4u.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
